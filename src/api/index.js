ee90d160d4a01872b61d079697c3aec8a8d59649
satishpanduru <satishpanduru2212@gmail.com>
2025-06-11 15:46:22 +0530

    updated index.js

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,7 @@ console.log("API Base URL:", process.env.REACT_APP_API_URL);
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL + '/api',
+  withCredentials: true,
 });
 
 // Automatically attach token to requests
